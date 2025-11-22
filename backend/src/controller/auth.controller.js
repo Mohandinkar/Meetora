397c64f33fba05858971bb91ee8f5a40a859526a
Mohandinkar <mohandinkar75@gmail.com>
2025-11-22 21:31:48 +0530

    onboarding endpoint

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -1,113 +1,177 @@
-import { upsertStreamUser } from '../lib/stream.js';
-import User from '../models/User.js';
-import jwt from 'jsonwebtoken';
+import { upsertStreamUser } from "../lib/stream.js";
+import User from "../models/User.js";
+import jwt from "jsonwebtoken";
 
-export async function signup(req,res){
-    const {email, password, fullName} = req.body;
+export async function signup(req, res) {
+  const { email, password, fullName } = req.body;
 
-    try {
-        
-        if(!email || !password || !fullName){
-            return res.status(400).json({message: "All fields are required"});
-        }
-
-        if(password.length < 6){
-            return res.status(400).json({message: "Password must be at least 6 characters"});
-        }
-        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        if(!emailRegex.test(email)){
-            return res.status(400).json({message: "Invalid email format"});
-        }
-
-        const existingUser = await User.findOne({email});
-        if(existingUser){
-            return res.status(400).json({message: "User already exists"});
-        }
-
-        //create the profile randomly 
-        const idx = Math.floor(Math.random() * 100) + 1;
-        const randomAvatar = `https://avatar.iran.liara.run/public/${idx}.png`;
-
-        const newUser = await User.create({
-            email,
-            password,
-            fullName,
-            profilePic: randomAvatar
-        });
-
-        //add user in STREAM also
-        try {
-            await upsertStreamUser({
-            id: newUser._id.toString(),
-            name:newUser.fullName,
-            image: newUser.profilePic || "",
-        })
-        console.log(`Stream user upserted successfully for, ${newUser.fullName}`);
-        } catch (error) {
-            console.error("Error in upserting Stream user during signup:", error);
-        }
-
-        const token = jwt.sign({userId: newUser._id}, process.env.JWT_SECRET, {
-            expiresIn: '7d'
-        });
-
-        res.cookie('jwt',token, {
-            maxAge: 7*24*60*60*1000,
-            httpOnly: true,
-            sameSite : 'strict',
-            secure: process.env.NODE_ENV === 'production'
-        })
-
-        res.status(201).json({success: true, user:newUser});
-
-    } catch (error) {
-        console.error("Error in signup:", error);
-        res.status(500).json({message: "Server error"});
+  try {
+    if (!email || !password || !fullName) {
+      return res.status(400).json({ message: "All fields are required" });
     }
+
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 characters" });
+    }
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ message: "Invalid email format" });
+    }
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    //create the profile randomly
+    const idx = Math.floor(Math.random() * 100) + 1;
+    const randomAvatar = `https://avatar.iran.liara.run/public/${idx}.png`;
+
+    const newUser = await User.create({
+      email,
+      password,
+      fullName,
+      profilePic: randomAvatar,
+    });
+
+    //add user in STREAM also
+    try {
+      await upsertStreamUser({
+        id: newUser._id.toString(),
+        name: newUser.fullName,
+        image: newUser.profilePic || "",
+      });
+      console.log(`Stream user upserted successfully for, ${newUser.fullName}`);
+    } catch (error) {
+      console.error("Error in upserting Stream user during signup:", error);
+    }
+
+    const token = jwt.sign({ userId: newUser._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    res.cookie("jwt", token, {
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+      httpOnly: true,
+      sameSite: "strict",
+      secure: process.env.NODE_ENV === "production",
+    });
+
+    res.status(201).json({ success: true, user: newUser });
+  } catch (error) {
+    console.error("Error in signup:", error);
+    res.status(500).json({ message: "Server error" });
+  }
 }
 
+export async function login(req, res) {
+  const { email, password } = req.body;
 
-export async function login(req,res){
-    const {email, password} = req.body;
-
-    try {
-        if(!email || !password){
-        return res.status(400).json({message: "Email and password are required"});
+  try {
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ message: "Email and password are required" });
     }
 
-    const user = await User.findOne({email});
+    const user = await User.findOne({ email });
 
-    if(!user){
-        return res.status(401).json({message: "Invalid credentials"}); 
+    if (!user) {
+      return res.status(401).json({ message: "Invalid credentials" });
     }
 
     const isMatch = await user.matchPassword(password);
 
-    if(!isMatch){
-        return res.status(401).json({message: "Invalid credentials"});
+    if (!isMatch) {
+      return res.status(401).json({ message: "Invalid credentials" });
     }
 
-    const token = jwt.sign({userId: user._id}, process.env.JWT_SECRET, {
-            expiresIn: '7d'
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
     });
 
-    res.cookie('jwt',token, {
-        maxAge: 7*24*60*60*1000,
-        httpOnly: true,
-        sameSite : 'strict',
-        secure: process.env.NODE_ENV === 'production'
-    })
+    res.cookie("jwt", token, {
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+      httpOnly: true,
+      sameSite: "strict",
+      secure: process.env.NODE_ENV === "production",
+    });
     res.status(200).json({ success: true, user });
-
-    } catch (error) {
-        console.error("Error in login:", error);
-        res.status(500).json({message: "Server error"});
-    }
-    
-
+  } catch (error) {
+    console.error("Error in login:", error);
+    res.status(500).json({ message: "Server error" });
+  }
 }
 
-export function logout(req,res){
-    res.clearCookie('jwt');
-    res.status(200).json({ success:true ,message: "Logged out successfully"});
+export function logout(req, res) {
+  res.clearCookie("jwt");
+  res.status(200).json({ success: true, message: "Logged out successfully" });
+}
+
+export async function onboard(req, res) {
+  try {
+    const userId = req.user._id;
+
+    const { fullName, nativeLanguage, learningLanguage, bio, location } = req.body;
+
+    if (
+      !fullName ||
+      !nativeLanguage ||
+      !learningLanguage ||
+      !bio ||
+      !location
+    ) {
+      return res.status(400).json({
+        message:
+          "All fields are required",
+        missingFields: [
+          !fullName && "fullName",
+          !nativeLanguage && "nativeLanguage",
+          !learningLanguage && "learningLanguage",
+          !bio && "bio",
+          !location && "location",
+        ].filter(Boolean),
+      });
+    }
+
+
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      {
+        fullName,
+        nativeLang: nativeLanguage,
+        learningLang: learningLanguage,
+        bio,
+        location,
+        isOnboarded: true,
+      },
+      { new: true }
+    );
+
+    //also update the name in STREAM
+
+   try{
+     await upsertStreamUser({
+      id: updatedUser._id.toString(),
+      name: updatedUser.fullName,
+      image: updatedUser.profilePic || "",
+    });
+    console.log(`Stream user upserted successfully for, ${updatedUser.fullName}`);
+   }catch(error){
+    console.error("Error in upserting Stream user during onboarding:", error);
+   }
+
+    if(!updatedUser){
+        return res.status(404).json({message: "User not found"});
+    }
+
+    res.status(200).json({success:true, user:updatedUser});
+
+  } catch (error) {
+
+    console.error("Error in onboarding:", error);
+    res.status(500).json({ message: "Server error" });
+  }
 }
--- backend/src/server.js
+++ backend/src/server.js
@@ -3,12 +3,15 @@ import dotenv from 'dotenv';
 dotenv.config();
 import authRoutes from './routes/auth.route.js';
 import {connectDB} from './lib/db.js';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 const port = process.env.PORT;
 
 app.use(express.json());
+app.use(cookieParser());
 app.use("/api/auth",authRoutes);
+
 
 app.listen(port,()=>{
     console.log(`server is running on port ${port}`);
