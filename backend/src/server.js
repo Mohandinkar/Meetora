2d0743b92c851c93166d8e07517a6cd111ef6ae0
Mohandinkar <mohandinkar75@gmail.com>
2026-01-08 12:15:45 +0530

    express serve the react build file with single server running

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,9 +7,12 @@ import chatRoutes from './routes/chat.route.js';
 import {connectDB} from './lib/db.js';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import path from 'path';
 
 const app = express();
 const port = process.env.PORT;
+
+const __dirname = path.resolve();
 
 app.use(cors({
     origin: "http://localhost:5173",
@@ -22,6 +25,15 @@ app.use(cookieParser());
 app.use("/api/auth",authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/chat",chatRoutes);
+
+//serve static files in production
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    // for any route other than the API routes, serve index.html 
+    app.get("*", (req,res)=>{
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+    })
+}
 
 app.listen(port,()=>{
     console.log(`server is running on port ${port}`);
